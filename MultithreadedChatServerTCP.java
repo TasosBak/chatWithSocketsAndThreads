ecc7f9f0a24996e3aeb1143b321445a8fce5852a
Anastasios Vakirtzis <anastasiosvakirtzis@gmail.com>
2020-06-07 11:15:22 +0300

    Cleanup

--- MultithreadedChatServerTCP.java
+++ MultithreadedChatServerTCP.java
@@ -20,16 +20,10 @@ public class MultithreadedChatServerTCP {
 			Socket dataSocket2 = connectionSocket.accept();
 			System.out.println("Received request from " + dataSocket2.getInetAddress());
 
-			// ServerThread sthread1 = new ServerThread(dataSocket1, dataSocket2);
-			// sthread1.start();
-
 			threads.add(threadCount, new ServerThread(dataSocket1, dataSocket2));
-
-			// ServerThread sthread2 = new ServerThread(dataSocket2, dataSocket1);
 
 			threadCount++;
 			threads.add(threadCount, new ServerThread(dataSocket2, dataSocket1));
-			// sthread2.start();
 
 			if (threadCount == 1) {
 				System.out.println("enough clients");
